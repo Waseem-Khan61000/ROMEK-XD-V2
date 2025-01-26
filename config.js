c89843382df2e571da8ecab8527cc8b55d7186ff
ROMEK XD <mdfaiyazraj786@gmail.com>
2025-01-26 18:19:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 ", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
 SUDO_NB: process.env.SUDO_NB || "", // 𝐀𝐃𝐃 𝐘𝐎𝐔𝐑 𝐍𝐔𝐌𝐁𝐄𝐑 𝐖𝐈𝐓𝐇 𝐂𝐎𝐔𝐍𝐓𝐑𝐘 𝐂𝐎𝐃𝐄
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //  𝐓𝐑𝐔𝐄 𝐎𝐑 𝐅𝐀𝐋𝐒𝐄
 OWNER_NUMBER: "",
