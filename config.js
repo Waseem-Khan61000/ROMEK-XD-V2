59a9ce11118d498f4119768177e03b882df8ea25
ROMEK XD <mdfaiyazraj786@gmail.com>
2025-02-02 11:55:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
+SESSION_ID: process.env.SESSION_ID || "zyx3WSaC#JtYsqH5YOZ1MPXXYGlll3A4m2mBQkmsL3xthJ1v6mLw", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
 SUDO_NB: process.env.SUDO_NB || "", // 𝐀𝐃𝐃 𝐘𝐎𝐔𝐑 𝐍𝐔𝐌𝐁𝐄𝐑 𝐖𝐈𝐓𝐇 𝐂𝐎𝐔𝐍𝐓𝐑𝐘 𝐂𝐎𝐃𝐄
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //  𝐓𝐑𝐔𝐄 𝐎𝐑 𝐅𝐀𝐋𝐒𝐄
-OWNER_NUMBER: "",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //  𝐓𝐑𝐔𝐄 𝐎𝐑 𝐅𝐀𝐋𝐒𝐄
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    OWNER_NUMBER: "",
 MODE: process.env.MODE || "public", //𝐏𝐑𝐈𝐕𝐀𝐓𝐄 𝐎𝐑 𝐏𝐔𝐁𝐋𝐈𝐂
 BOT_NAME: process.env.BOT_NAME || "ROMEK-XD-V2",
 // add bot namw here for menu
