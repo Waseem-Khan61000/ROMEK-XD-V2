8b1b7ca4d56159124e72926263a8d1c584782453
Waseem Shahzad <waseemzadibaloch@gmail.com>
2025-02-14 21:59:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ADD YOUR SESSION ID", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
+SESSION_ID: process.env.SESSION_ID || "PiYWSIYB#wAIHTWymhuzyZN4BLnM8oYgp5_6RP1IT4KX5AWz-iuw", // 𝐀𝐃𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 
 SUDO_NB: process.env.SUDO_NB || "", // 𝐀𝐃𝐃 𝐘𝐎𝐔𝐑 𝐍𝐔𝐌𝐁𝐄𝐑 𝐖𝐈𝐓𝐇 𝐂𝐎𝐔𝐍𝐓𝐑𝐘 𝐂𝐎𝐃𝐄
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //  𝐓𝐑𝐔𝐄 𝐎𝐑 𝐅𝐀𝐋𝐒𝐄
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
@@ -15,8 +15,8 @@ BOT_NAME: process.env.BOT_NAME || "ROMEK-XD-V2",
 // add bot namw here for menu
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "919341378016",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923311489610",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ROMEK-XD",
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʀᴏᴍᴇᴋ-xᴅ-ᴠ2*",
+OWNER_NAME: process.env.OWNER_NAME || "WASEEM-TECHX",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Waseem-techx*",
 };
